467ec7c582a3d5946fd0d599991359d1d517edd5
Nat <Natalieeeeeeeeeeeeeeeeee@gmail.com>
2019-02-01 10:12:59 -0700

    Add tabs
    Use notebook class to add state tabs to GUI

--- testgui.py
+++ testgui.py
@@ -1,57 +1,44 @@
 ###Python File for Building GUI Visual Elements###
 from Tkinter import Tk, Button, Label
+import ttk
 from functools import partial
-class GUI:
-    def __init__(self, master):
-        ##Set Master
-        self.master = master
-        ##Set GUI Title
-        master.title("SDRC URC Driver Station")
-        ##TODO:  Add Window Icon
-        ##Add Status Labels
-        self.connection_header = Label(master, text = "Rover Status", bg = 'gray', font=(None, 30))
-        self.connection_header.grid(row=0, column=4)
-        self.connection_label = Label(master, text = "Disconnected", bg = 'red', font=(None, 30))
-        self.connection_label.grid(row=1, column=4)
-        #self.vbat_label = Label(master, text="VBat = ?V")
-        #self.vbat_label.grid(row=0, column = 1)
-        ##Add Enable/Disable Buttons
-        #Row 1
-        #Button 1
-        self.button1_label = Label(master, text = "<button> Status", bg = 'gray', font=(None, 20))
-        self.button1_label.grid(row=2, column=2)
-        self.enable_button = Button(master, text="Enable", bg='green', width = 10, height = 3, command=partial(self.switch_button_status, text="button 1 enabled", enabled=True, label=self.button1_label))
-        self.enable_button.grid(row=3, column = 1)
-        self.disable_button = Button(master, text="Disable", bg='red', width = 10, height = 3, command=partial(self.switch_button_status, text="button 1 enabled", enabled=False, label=self.button1_label))
-        self.disable_button.grid(row = 3, column = 3)
-        #Button 2
-        self.button2_label = Label(master, text = "<button 2> Status", bg = 'gray', font=(None, 20))
-        self.button2_label.grid(row=2, column=6)
-        self.enable_button2 = Button(master, text="Enable", bg='green', width = 10, height = 3, command=partial(self.switch_button_status, text="button 2 enabled", enabled=True, label=self.button2_label))
-        self.enable_button2.grid(row=3, column = 5)
-        self.disable_button2 = Button(master, text="Disable", bg='red', width = 10, height = 3, command=partial(self.switch_button_status, text="button 2 enabled", enabled=False, label=self.button2_label))
-        self.disable_button2.grid(row = 3, column = 7)
-        #Row 2
-        #Button 3
-        self.button3_label = Label(master, text = "<button 3> Status", bg = 'gray', font=(None, 20))
-        self.button3_label.grid(row=5, column=2)
-        self.enable_button3 = Button(master, text="Enable", bg='green', width = 10, height = 3, command=partial(self.switch_button_status, text="button 3 enabled", enabled=True, label=self.button3_label))
-        self.enable_button3.grid(row=6, column = 1)
-        self.disable_button3 = Button(master, text="Disable", bg='red', width = 10, height = 3, command=partial(self.switch_button_status, text="button 3 enabled", enabled=False, label=self.button3_label))
-        self.disable_button3.grid(row = 6, column = 3)
-        #Button 4
-        self.button4_label = Label(master, text = "<button 4>Status", bg = 'gray', font=(None, 20))
-        self.button4_label.grid(row=5, column=6)
-        self.enable_button4 = Button(master, text="Enable", bg='green', width = 10, height = 3, command=partial(self.switch_button_status, text="button 4 enabled", enabled=True, label=self.button4_label))
-        self.enable_button4.grid(row=6, column = 5)
-        self.disable_button4 = Button(master, text="Disable", bg='red', width = 10, height = 3, command=partial(self.switch_button_status, text="button 4 enabled", enabled=False, label=self.button4_label))
-        self.disable_button4.grid(row = 6, column = 7)
-    #Button 1 enable/disable data
-    def switch_button_status(self, text, enabled, label):
-        color = 'green' if enabled else 'red'
-        label.config(text=text)
-        label.config(bg = color)
-if __name__ == '__main__':
-    root = Tk()
-    gui = GUI(root)
-    root.mainloop()
+
+window = Tk()
+window.title("SDRC URC Driver Station")
+tab_control = ttk.Notebook(window, width=800, height=800)
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+tab3 = ttk.Frame(tab_control)
+tab_control.add(tab1, text='State 1')
+tab_control.add(tab2, text='State 2')
+tab_control.add(tab3, text='State 3')
+
+#tab 1
+lbl1 = Label(tab1, text= 'button 1 <command>')
+lbl1.grid(column=2, row=2)
+button1_label = Label(tab1, text = " Status ", bg = 'gray', font=(None, 20))
+button1_label.grid(row=4, column=2)
+enable_button = Button(tab1, bg = 'red', text="Enable", width = 10, height = 3, command=lambda: switch_button_status(text="button 1 enabled", enabled=True, label=button1_label))
+enable_button.grid(row=4, column=1)
+disable_button = Button(tab1, bg = 'red', text="Disable", width = 10, height = 3, command=lambda: switch_button_status(text="button 1 enabled", enabled=False, label=button1_label))
+disable_button.grid(row = 4, column = 3)
+
+
+
+
+
+
+
+#tab 2
+lbl2 = Label(tab2, text= 'something cool here')
+lbl2.grid(column=0, row=0)
+tab_control.pack(expand=1, fill='both')
+
+
+def switch_button_status(text, enabled, label):
+    color = 'green' if enabled else 'red'
+    label.config(text=text)
+    label.config(bg=color)
+
+
+window.mainloop()
